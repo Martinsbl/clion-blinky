4953863ecc165ccf1d135c3f42f943ae692acea8
Martinsbl <martinsbl@gmail.com>
2016-09-13 19:59:43 +0200

    update

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Simple example to get started with Nordic's SDKs in CLion
+This is a simple example showing you how to setup and configure CLion and get started with Nordic's SDKs. It only shows how to do it in Windows and only the basic setps. 
+
+# You'll need:
+* [CLion](https://www.jetbrains.com/clion/).
+* [TDM-GCC compiler](http://tdm-gcc.tdragon.net/).
+* [SDK from Nordic](http://www.nordicsemi.com/eng/nordic/Products/nRF52-DK/nRF5-SDK-zip/54283). I used SDK V11.
+
--- toolchain.cmake
+++ toolchain.cmake
@@ -1,13 +1,16 @@
 INCLUDE(CMakeForceCompiler)
 SET(CMAKE_SYSTEM_NAME Generic)
-CMAKE_FORCE_C_COMPILER("C:/Program Files (x86)/GNU Tools ARM Embedded/4.9 2015q3/bin/arm-none-eabi-gcc.exe" GNU)
-#add_compile_options(-Wno-deprecated-declarations)
-
+#Make sure this is the absolute path to your GNU compiler install folder. Remember to get the version number correct.
+CMAKE_FORCE_C_COMPILER("C:/Program Files (x86)/GNU Tools ARM Embedded/5.4 2016q2/bin/arm-none-eabi-gcc.exe" GNU)
+add_compile_options(-Wno-deprecated-declarations) # Get rid of "The CMAKE_FORCE_C_COMPILER macro is deprecated." warning at compilation
 
 SET(CMAKE_C_FLAGS "-std=gnu99 -mcpu=cortex-m0 -mthumb -mabi=aapcs -g3 -mfloat-abi=soft -ffunction-sections -fdata-sections -fno-strict-aliasing -fno-builtin --short-enums")
 SET(CMAKE_ASM_FLAGS "-x assembler-with-cpp")
 
+
+set(SDK_ROOT_FOLDER "C:/Kode/Nordic/SDK_11")
+
 SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/linker_script.ld)
-SET(TEMPLATE_PATH "../../../components/toolchain/gcc/Makefile.windows")
+SET(TEMPLATE_PATH "${SDK_ROOT_FOLDER}/components/toolchain/gcc/Makefile.windows")
 SET(CMAKE_EXE_LINKER_FLAGS "-Xlinker -Map=${LISTING_DIRECTORY}/${OUTPUT_FILENAME}.map -mthumb -mabi=aapcs -L ${TEMPLATE_PATH} -T${LINKER_SCRIPT} -mcpu=cortex-m0 -Wl,--gc-sections  --specs=nano.specs -lc -lnosys")
 
